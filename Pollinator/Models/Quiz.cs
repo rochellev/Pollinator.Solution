d548aea4179eea6a8f4500c2ed206357d93635e6
Lisa Jensen and Rochelle Roberts <lisa+rochelle@>
2019-08-26 09:22:05 -0700

    add quiz and question models

---
+++ Pollinator/Models/Quiz.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+namespace Pollinator.Models
+{
+    public class Quiz
+    {
+        public int QuizId {get; set;}
+        public string Name {get; set;}
+        public string Description {get; set;}
+        public int AuthorId {get; set;}
+        public virtual ICollection<QuizQuestion> Questions {get;set;}
+
+        public Quiz()
+        {
+            this.Questions = new HashSet<QuizQuestion>();
+        }
+    }
+}
