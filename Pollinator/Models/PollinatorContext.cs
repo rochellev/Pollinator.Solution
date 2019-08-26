77d00cec8833176becc89c40168dd0c04e7b1aca
Lisa Jensen and Rochelle Roberts <lisa+rochelle@>
2019-08-26 09:40:09 -0700

    add migrations

--- Pollinator/Models/PollinatorContext.cs
+++ Pollinator/Models/PollinatorContext.cs
@@ -3,12 +3,12 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Pollinator.Models
 {
-  public class PollinatorContext
+  public class PollinatorContext : IdentityDbContext<ApplicationUser>
   {
     public virtual DbSet<Quiz> Quizzes { get; set; }
     public DbSet<Question> Questions { get; set; }
     public DbSet<QuizQuestion> QuizQuestion { get; set; }
 
-    public PollinatorContext(DbContextOptions options){ }
+    public PollinatorContext(DbContextOptions options): base(options){ }
   }
 }
