9c98ca4f88036b1cdf73e394e3dab4960b709b1e
Lisa Jensen <leese_38@yahoo.com>
2019-08-28 16:25:03 -0700

    add migration fix

--- Pollinator/Controllers/ChartController.cs
+++ Pollinator/Controllers/ChartController.cs
@@ -21,7 +21,7 @@ namespace Pollinator.Controllers
             List<DataPoint> dataPoints = new List<DataPoint>{
                 new DataPoint(10, 20),
                 new DataPoint(20, 70),
-                new DataPoint(30, 10),
+                // new DataPoint(30, 10),
 
             };
 
